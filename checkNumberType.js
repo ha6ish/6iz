aaf53ace352646f479d11a1d96be8069c2096fc1
ha6ish <harish_kumar.d@hotmail.com>
2026-02-03 20:40:21 +0530

    Session 2
    Check Positive Negative

---
+++ checkNumberType.js
@@ -0,0 +1,13 @@
+let num
+function checkNumberType(num){
+
+if( num> 0){
+    return  "Positive Number"
+
+}else if(num< 0){
+    return  "Negative number"
+}else{
+
+    return "zero" 
+}}
+console.log(checkNumberType(5));
