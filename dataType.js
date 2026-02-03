08a9234a95e8907f436316d74a8196b357cbb13e
ha6ish <harish_kumar.d@hotmail.com>
2026-02-03 20:24:23 +0530

    Session 1

---
+++ dataType.js
@@ -0,0 +1,15 @@
+var firstName = "harish"
+var companyName = "Google"
+var mobileNumber  = 9962876944
+//var isAutomation = true
+let knowsAutomation = true
+
+console.log("my name is"+typeof(firstName))
+console.log("my company name is"+typeof(companyName))
+console.log("my mobile number is" +typeof(mobileNumber))
+//console.log(true)
+if (knowsAutomation){
+    console.log("User knows automation")
+}else{
+    console.log("learn more")
+}
