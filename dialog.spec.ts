23e3c9c96fec77a28f350eccf7933c3cf2a3bb7e
ha6ish <harish_kumar.d@hotmail.com>
2026-02-24 20:40:29 +0530

    Automating Alert & Frame Interactions

---
+++ dialog.spec.ts
@@ -0,0 +1,28 @@
+// import {test} from '@playwright/test';
+// import { ok } from 'node:assert';
+
+// test("dialog",async({page})=>{
+//     await page.goto("https://www.w3schools.com/js/tryit.asp?filename=tryjs_confirm")
+//     await page.frameLocator("#iframeResult").getByRole("button", { name: "Try it" }).click()
+//     page.on("dialog",async dialog=>{
+//         console.log(dialog.message())
+//         await dialog.accept()
+//     })})
+
+import { test } from '@playwright/test';
+
+test("dialog", async ({ page }) => {
+
+    await page.goto("https://www.w3schools.com/js/tryit.asp?filename=tryjs_confirm");
+
+    // Register dialog handler BEFORE click
+    page.once("dialog", async dialog => {
+        console.log(dialog.message());
+        await dialog.accept(); // or dialog.dismiss();
+    });
+
+    await page.frameLocator("#iframeResult")
+        .getByRole("button", { name: "Try it" })
+        .click();
+
+});
